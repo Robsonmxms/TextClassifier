43a432657048dbe3d0e38002e1c638bc6d35d446
Robson Lima Lopes <robson060814@hotmail.com>
2023-05-15 09:49:07 -0300

    Initial Commit

---
+++ MyTextClassifier/MyTextClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTextClassifierApp.swift
+//  MyTextClassifier
+//
+//  Created by Robson Lima Lopes on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTextClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
